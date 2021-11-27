d993d814020360bb7e67853660c0469a2dbbad54
justicemubayiwa <j.mubayiwa@gmail.com>
2021-11-27 14:23:57 -0500

    nesting components

---
+++ src/CommentDetail.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import faker from 'faker'
+
+function CommentDetail() {
+    return (
+        <div className="comment">
+            <a className="avatar" href="/">
+                <img alt="avatar" src={faker.image.avatar()} />
+            </a>
+            <div className="content">
+                <a className="author" href ="/">
+                    Sam
+                </a>
+            <div className="metadata">
+                <span className="date">
+                Today at 6:00PM
+                </span>
+            </div>
+            <div className="text">
+                Nice Blog Post
+            </div>
+        </div>
+      </div>
+    )
+}
+
+export default CommentDetail
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetail'
 
 function App() {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a className="avatar" href="/">
-          <img alt="avatar"/>
-        </a>
-        <div className="content">
-          <a className="author" href ="/">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">
-              Today at 6:00PM
-            </span>
-          </div>
-          <div className="text">
-            Nice Blog Post
-          </div>
-        </div>
-      </div>
+      <CommentDetail/>
     </div>
   )
 }
