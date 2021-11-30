fc496dafb624224ca1afc6baedf220ed66fd097d
justicemubayiwa <j.mubayiwa@gmail.com>
2021-11-30 17:45:17 -0500

    css styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import './SeasonDisplay.css' 
 
-function SeasonDisplay() {
+const seasonConfig = {
+    summer: {
+        text: 'Lets go outside',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'It is too cold',
+        iconName: 'snowflake'
+    }
+}
+function getSeason(lat, month){
+if (month > 2 && month < 9){
+    return lat > 0 ? "summer" : "winter"
+} else {
+    return lat > 0 ? "winter" : "summer"
+}
+}
+
+function SeasonDisplay(props) {
+    const season = getSeason(props.lat, new Date().getMonth())
+    const {text, iconName} = seasonConfig[season]
+   
     return (
-        <div>
-            Season
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     )
 }
