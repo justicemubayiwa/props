47bff6f52082b0221d6b8532f020667841515a35
justicemubayiwa <j.mubayiwa@gmail.com>
2021-11-30 17:52:31 -0500

    loader

---
+++ src/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+function Spinner() {
+    return (
+            <div class="ui active dimmer">
+                <div class="ui big text loader">Loading...</div>
+            </div>
+    )
+}
+
+export default Spinner
